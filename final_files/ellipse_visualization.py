160836041cb21c9bfe5dfa9ec00599b6baf8edc1
Arunabh Ghosh <arunabhghosh98@gmail.com>
2020-08-29 00:36:18 -0700

    Add movie code

--- final_files/ellipse_visualization.py
+++ final_files/ellipse_visualization.py
@@ -1,8 +1,8 @@
 # Parameters
-alpha = 2
+alpha = 1.5
 dataset = 'u2'
 pos_folder = 'results/'
-positions_file = 'fp_u2_alpha_2_numpoints_40000_drag_5.0.npy'
+positions_file = 'fp_u2_alpha_1.0_numpoints_50000_drag_30.0.npy'
 import cv2
 from matplotlib.patches import Ellipse
 import matplotlib.pyplot as plt
@@ -23,10 +23,32 @@ if dataset == 'u2':
     orientation = rescale(orientation, 0.5, anti_aliasing=True)
     anisotropy = anisotropy / 65535*10
     orientation = orientation / 18000*np.pi
-    anisotropy = np.ones_like(anisotropy) + 1
+#     anisotropy = np.ones_like(anisotropy) + 1
     USmooth, VSmooth = anisotropy*np.cos(orientation), anisotropy*np.sin(orientation)
     VSmooth = VSmooth*-1
     orientation = np.arctan2(VSmooth, USmooth)
+
+def nanRobustBlur(I, dim):
+    V=I.copy()
+    V[I!=I]=0
+    VV=cv2.blur(V,dim)   
+    W=0*I.copy()+1
+    W[I!=I]=0
+    WW=cv2.blur(W,dim)    
+    Z=VV/WW
+    return Z
+
+def return_smooth(orientation, anisotropy):
+    U, V =  anisotropy*np.cos(2 * orientation), anisotropy*np.sin(2 * orientation)
+    USmooth = nanRobustBlur(U, (5, 5)) # plot smoothed vector field
+    VSmooth = nanRobustBlur(V, (5, 5)) # plot smoothed vector field
+    azimuthSmooth = (0.5*np.arctan2(VSmooth,USmooth)) % np.pi
+    RSmooth = np.sqrt(USmooth**2+VSmooth**2)
+    
+    return RSmooth, azimuthSmooth
+
+un_avg_anisotropy = anisotropy
+anisotropy, orientation = return_smooth(orientation, anisotropy)
 
 final_positions = np.load(pos_folder + positions_file)
 final_positions = final_positions.astype(int)
@@ -40,14 +62,14 @@ for i in range(len(final_positions)):
 major_axis_len = []
 minor_axis_len = []
 orientation_list = []
-scale_ellipse_major = alpha*1.2
-scale_ellipse_minor = alpha*0.8
+scale_ellipse_major = alpha*1.3
+scale_ellipse_minor = alpha*0.7
 
 for p in within_boundary_pos:
     orientation_list.append(orientation[p[0], p[1]])
     anisotropy_value = anisotropy[p[0], p[1]]
 
-    if abs(anisotropy_value) < 1.6:
+    if abs(anisotropy_value) < 1.1:
         major_axis_len.append(1*0.01)
         minor_axis_len.append(abs(anisotropy_value)*0.01)
     else:
@@ -71,9 +93,9 @@ fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
 fig.set_figheight(30)
 fig.set_figwidth(30)
 if dataset == 'u2':
-    plt.imshow(anisotropy, cmap=cmapImage)
+    plt.imshow(un_avg_anisotropy, cmap=cmapImage)
 else:
-    plt.imshow(anisotropy, cmap=cmapImage, origin='lower')
+    plt.imshow(un_avg_anisotropy, cmap=cmapImage, origin='lower')
 k = 0
 for e in ells:
     ax.add_artist(e)
